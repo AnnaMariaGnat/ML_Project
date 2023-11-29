22c32cd88a2406e1e289617a5db86bf50003de77
BuildTools <unconfigured@null.spigotmc.org>
2023-11-29 22:03:55 +0100

    Fixed complex eigenvalues and eigenvectors

--- lda_lib.py
+++ lda_lib.py
@@ -5,6 +5,7 @@ class lda_ajp:
         Anna Maria Gnat, Josefine Nyeng and Pedro Prazeres
         for the Machine Learning course at ITU CPH '''
     
+
     def __init__(self, X, y):
         ''' Initializes the class with the data and classes '''
         self.X = X # Data
@@ -14,17 +15,20 @@ class lda_ajp:
         # Central point (mean) of the means of each class (used several times so just initialize it once)
         self.central_point = self.central_point()
 
+
     def class_means(self):
         ''' Finds the mean of each class' data points and saves them as a numpy array '''
         class_means = np.array(
             [np.mean(self.X[self.y == i], axis=0) # Mean of all data points in class i...
                      for i in np.unique(self.y)]) # ...for each class in y.
         return class_means
-    
+
+
     def central_point(self):
         ''' Finds the central point of the data and saves it as a numpy array '''
         central_point = np.mean(self.class_means, axis=0) # Mean of all class means.
         return central_point
+
 
     def scatter_within(self):
         ''' Finds the within-class scatter matrix and saves it as a numpy array '''
@@ -36,6 +40,7 @@ class lda_ajp:
         scatter_within = scatter_within / self.X.shape[0] # Divide by the number of data points to scale the matrix.
         return scatter_within
 
+
     def scatter_between(self):
         ''' Finds the between-class scatter matrix and saves it as a numpy array '''
         scatter_between = np.zeros((self.X.shape[1], self.X.shape[1])) # Initialize scatter matrix with zeros.
@@ -46,7 +51,8 @@ class lda_ajp:
                 (self.class_means[i] - self.central_point)) # ...and the transpose of the same difference.
         scatter_between = scatter_between / self.X.shape[0] # Divide by the number of data points to scale the matrix.
         return scatter_between
-    
+
+
     def lda_matrix(self):
         ''' Finds the LDA matrix and saves it as a numpy array '''
         scatter_within = self.scatter_within() # Find the within-class scatter matrix.
@@ -55,26 +61,31 @@ class lda_ajp:
             np.linalg.inv(scatter_within), # ...the inverse of the within-class scatter matrix...
             scatter_between) # ...with the between-class scatter matrix.
         return lda_matrix
-    
+
+
     def lin_discs(self):
         ''' Finds the linear discriminants and saves them as numpy arrays '''
         lda_matrix = self.lda_matrix() # Find the LDA matrix.
-        eigenvals, eigenvects = np.linalg.eigh(lda_matrix) # Find the eigenvalues and eigenvectors of the LDA matrix.
+        eigenvals, eigenvects = np.linalg.eig(lda_matrix) # Find the eigenvalues and eigenvectors of the LDA matrix.
+        eigenvals = np.real(eigenvals) # Find and remove complex eigenvalues.
+        eigenvects = np.real(eigenvects) # Find and remove complex eigenvectors.
         idx = eigenvals.argsort() # Sort the eigenvalues and eigenvectors (ascending order).
         idx = idx[::-1] # Reverse the order of the sorted eigenvalues and eigenvectors (descending order).
         eigenvals = eigenvals[idx] # Sort the eigenvalues according to the sorted indices.
         eigenvects = eigenvects[:,idx] # Sort the eigenvectors according to the sorted indices.
         return eigenvals, eigenvects
-       
+
+
     def projection_matrix(self, n_ld=2):
-        ''' Finds the transformed data and saves it as a numpy array '''
+        ''' Creates a matrix of the projected data with n linear discriminants and saves it as a numpy array '''
         top_eigenvects = self.main_lds(n_ld) # Find the main linear discriminants.
         X_lda = np.dot(top_eigenvects.T, self.X.T) # Project the data onto the main linear discriminants.
         X_lda = X_lda.T # Transpose the data to get the correct shape.
         return X_lda
 
+
     def main_lds(self, n_ld=2):
-        ''' Finds the main linear discriminants and saves them as a numpy array '''
+        ''' Returns the n main linear discriminants as numpy arrays '''
         _, vects = self.lin_discs() # Find the eigenvectors of the LDA matrix.
         main_lds = vects[:, :n_ld] # Find the main linear discriminants by taking the first n_ld eigenvectors.
         return main_lds
